176138dcbf887da42ab09e3b8e6148ef6cec8945
Sumnicky99 <sumnicky99@gmail.com>
2024-08-02 14:52:08 +0300

    created a methods for setting and checking hashed pass  in  user.py

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,4 +1,4 @@
-# Importing the necessary libraries and modules
+# Import necessary libraries and modules
 
 # Import the SQLAlchemy instance from the app package
 from app import db
@@ -12,6 +12,12 @@ from flask_login import UserMixin
 
 # Import time for handling time-related functions
 from time import time
+
+# Import jwt for JSON Web Token operations
+import jwt
+
+# Import functions for hashing passwords
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # User class inherits from SQLAlchemy's Model and Flask-Login's UserMixin
 class User(UserMixin, db.Model):
@@ -42,3 +48,33 @@ class User(UserMixin, db.Model):
     
     # One-to-one relationship with LeaveRequest
     leave_request = db.relationship('LeaveRequest', uselist=False, backref='user', lazy=True)
+
+    # Methods for setting and checking password hashes
+    def set_password(self, password):
+        # Set the hashed password for the user
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        # Check if the provided password matches the hashed password
+        return check_password_hash(self.password_hash, password)
+    
+    # Generate JWT token for the user
+    def get_reset_password_token(self, expires_in=600):
+        # Create a JWT token for password reset with an expiration time
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        # Verify the JWT token and return the corresponding user
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return None
+        return User.query.get(id)
+    
+    # String representation of the User object
+    def __repr__(self):
+        # Provide a string representation for debugging and logging
+        return f'<User {self.username}>'

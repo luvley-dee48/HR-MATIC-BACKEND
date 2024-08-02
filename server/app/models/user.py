556f21b725ba0300bfaac7d60b8e03a1aa5f0061
Sumnicky99 <sumnicky99@gmail.com>
2024-08-02 10:40:15 +0300

    basic imports in user.py

---
+++ server/app/models/user.py
@@ -0,0 +1,9 @@
+# Import the SQLAlchemy instance that will be used to define models and interact with the database
+from app import db
+
+
+#importing the  UserMixin (install this in your laptop pip install flask-login )
+from flask_login import UserMixin
+
+
+    

182552772001d247aafaf5efa02ee18ec41a066e
Sumnicky99 <sumnicky99@gmail.com>
2024-08-05 13:36:53 +0300

    importing date in seed.py

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from models import db
 
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'  # Example for SQLite
